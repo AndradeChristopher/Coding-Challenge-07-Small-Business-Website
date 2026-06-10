77b62fe7d7b21e451fb30c415cea4466a5cdde98
Christopher Andrade <christopherandrade@usf.edu>
2026-06-10 14:44:21 -0400

    Update cc_7.js

--- js/cc_7.js
+++ js/cc_7.js
@@ -1,1 +1,11 @@
+const headlineInput = document.getElementById("headlineInput");
+const updateButton = document.getElementById("updateButton");
+const ctaHeadline = document.getElementById("ctaHeadline");
 
+updateButton.addEventListener("click", function () {
+
+    if (headlineInput.value.trim() !== "") {
+        ctaHeadline.textContent = headlineInput.value;
+    }
+
+});
